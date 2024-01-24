2c6ca59a707527ea0a47c8380dd8eb051be4a62c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-24 10:29:48 +0430

    adding src file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import trollImge from "../images/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={trollImge}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
